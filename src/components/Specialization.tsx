6f7d6f5fea5f9dcb8e4c235d4400d8a531da2c0f
wrogoz <wrogoz@gmail.com>
2019-11-25 23:15:02 +0100

    btn added with dummy data

--- src/components/Specialization.tsx
+++ src/components/Specialization.tsx
@@ -1,23 +1,50 @@
 import React from 'react'
 import {GridCell} from '@rmwc/grid'
 import '@material/layout-grid/dist/mdc.layout-grid.css';
+import {Button} from '@rmwc/button'
+import '@material/button/dist/mdc.button.css';
+import styled from 'styled-components'
+
 
 export default class Specialization extends React.Component<{},{}>{
     render(){
         return(
         <>
-            <GridCell span={4} phone={6}>Neurolog</GridCell>
-            <GridCell span={4} phone={6}>Ortopedia</GridCell>
-            <GridCell span={4} phone={6}>Pediatra</GridCell>
-            <GridCell span={4} phone={6}>Neurolog</GridCell>
-            <GridCell span={4} phone={6}>Ortopedia</GridCell>
-            <GridCell span={4} phone={6}>Pediatra</GridCell>
-            <GridCell span={4} phone={6}>Neurolog</GridCell>
-            <GridCell span={4} phone={6}>Ortopedia</GridCell>
-            <GridCell span={4} phone={6}>Pediatra</GridCell>
+            <SpecializeCell desktop={3} span={4} phone={6} >
+                <Button label="Unelevated" unelevated />
+            </SpecializeCell>
+            <SpecializeCell desktop={3} span={4} phone={6} >
+                <Button label="Unelevated" unelevated />
+            </SpecializeCell>
+            <SpecializeCell desktop={3} span={4} phone={6} >
+                <Button label="Unelevated" unelevated />
+            </SpecializeCell>
+            <SpecializeCell desktop={3} span={4} phone={6} >
+                <Button label="Unelevated" unelevated />
+            </SpecializeCell>
+            <SpecializeCell desktop={3} span={4} phone={6} >
+                <Button label="Unelevated" unelevated />
+            </SpecializeCell>
+            <SpecializeCell  desktop={3} span={4} phone={6} >
+                <Button label="Unelevated" unelevated />
+            </SpecializeCell>
+            <SpecializeCell desktop={3} span={4} phone={6} >
+                <Button label="Unelevated" unelevated />
+            </SpecializeCell>
+            <SpecializeCell desktop={3} span={4} phone={6} >
+                <Button label="Unelevated" unelevated />
+            </SpecializeCell>
+            
+            
          
         </>    
         )
     }
 }
+
+
+const SpecializeCell = styled(GridCell as any)`
+    display:flex;
+    justify-content:center;
+`
 
