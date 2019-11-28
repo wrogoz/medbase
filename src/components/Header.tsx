3226c9cb544e6f9a22a67144863e436daa35a215
wrogoz <wrogoz@gmail.com>
2019-11-28 13:12:15 +0100

    creating new header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,51 @@
 import React from 'react'
 import styled from 'styled-components';
-export default class Header extends React.Component<{},{}>{
+
+interface HeaderElements{
+    title:string;
+    subtitle:string;
+}
+
+export default class Header extends React.Component<HeaderElements,{}>{
     render(){
         return(
             <StyledHeader>
-                <h1>MEDBASE</h1>
+                
+                <AppTitle>
+                    {this.props.title}
+                </AppTitle>
+
+                <SubTitle>
+                {this.props.subtitle}
+                </SubTitle>
+
             </StyledHeader>
         )
     }
 }
 
-const StyledHeader = styled.div`
-    height:12vh;
+const StyledHeader = styled.header`
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    font-family: 'Montserrat', sans-serif;
+`
+const AppTitle = styled.h1`
+    margin:0;
+    padding:36px 0 12px 16px;;
+    font-size:36px;
+    font-style:normal;
+    letter-spacing:0.5em;
+    text-transform:uppercase;
+    font-weight:400;
+    
+
+`
+const SubTitle = styled.h2`
+    font-size:16px;
+    margin:0;
+    letter-spacing:0.1em;
+    color:#9e9a9a;
+    font-weight:400;
+
 `
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,56 +1,19 @@
 import React from 'react'
-import Header from './../Header'
-import {GridCell,Grid, GridCellProps} from '@rmwc/grid'
-import Specialization from '../Specialization';
-import styled from 'styled-components';
 import '@material/layout-grid/dist/mdc.layout-grid.css';
-
+import Header from './../Header';
 export default class Layout extends React.Component<{},{}>{
     render(){
         return(
-            <Grid>
-
-               
-                    <HeaderCell span={12}>
-                        <Header/>
-                    </HeaderCell>
-               
-
-              
-                    <ChooseCell span={12}>
-                    <h2>Wybierz:</h2>
-                    </ChooseCell>
-               
-
-               
-                    <Specialization/>
-                  
-
-                
-                    <GridCell span={12}>
-                    <p>go to the next page</p>
-                    </GridCell>
-               
-
-
-       
-       
-        
-       
-            </Grid>
+            <div>
+                <Header
+                    title="medbase"
+                    subtitle="Wyszukiwarka lekarzy NFZ"
+                />
+                <p>core</p>
+                <p>footer</p>
+            </div>
         )
     }
 }
 
-const HeaderCell = styled (GridCell as React.ComponentType<GridCellProps>
-  )`
-    display:flex;
-    justify-content:flex-end;
-    padding-right:15px;
-`
 
-const ChooseCell = styled (GridCell as React.ComponentType<GridCellProps>
-    )`
-    display:flex;
-    justify-content:center;
-`
